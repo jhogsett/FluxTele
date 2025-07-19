c45ca44c8e9d213580a7d90a53e15c7e7e5ec639
Jerry Hogsett <jhogsett@gmail.com>
2025-07-19 16:08:30 -0700

    adjust brightness

--- include/hardware.h
+++ include/hardware.h
@@ -18,7 +18,6 @@
 #define DEVICE_VARIANT_GREEN_DISPLAY    // Original green display version
 // #define DEVICE_VARIANT_RED_DISPLAY      // Red display version with dimmer LEDs
 // #define DEVICE_VARIANT_AMBER_DISPLAY      // Red display version with dimmer LEDs
-#define DEVICE_VARIANT_AMBER_DISPLAY      // Red display version with dimmer LEDs
 
 // Device-specific scaling factors
 #if defined(DEVICE_VARIANT_RED_DISPLAY)
@@ -32,7 +31,7 @@
     #define SIGNAL_METER_BRIGHTNESS_DIVISOR 4      // No scaling for green display
     #define PANEL_LED_BRIGHTNESS_DIVISOR 1         // Divide by 2 for green display
 #else // Green display variant (default): Original brightness
-    #define PANEL_LOCK_LED_FULL_BRIGHTNESS 511
+    #define PANEL_LOCK_LED_FULL_BRIGHTNESS 255
     #define SIGNAL_METER_BRIGHTNESS_DIVISOR 1      // No scaling for green display
     #define PANEL_LED_BRIGHTNESS_DIVISOR 1         // Divide by 2 for green display
 #endif
