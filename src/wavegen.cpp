d4b769e08dbdb1b96ca738da9c9d10cebd520437
Jerry Hogsett <jhogsett@gmail.com>
2025-07-18 21:29:04 -0700

    fix concept conflict

--- src/wavegen.cpp
+++ src/wavegen.cpp
@@ -12,8 +12,8 @@ void WaveGen::set_frequency(float frequency, bool main){
 	// if(frequency != _frequency || _main != main){
 		// Serial.println("set freq");
 		_sig_gen->setFrequency((MD_AD9833::channel_t)(main ? 0 : 1), frequency);
-		// _frequency = frequency;
-		// _main = main;
+		_frequency = frequency;
+		_main = main;
 	// }
 }
 
@@ -21,7 +21,7 @@ void WaveGen::set_active_frequency(bool main){
 	// if(_main != main){
 		// Serial.println("set act freq");
 		_sig_gen->setActiveFrequency((MD_AD9833::channel_t)(main ? 0 : 1));
-		// _main = main;
+		_main = main;
 	// }
 }
 
